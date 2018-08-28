8a44eccfecc36c6fe222bab68f14f65bf3b105b4
Saul Ponce <sponce@Sauls-MacBook-Pro.local>
2018-08-27 21:45:39 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular4';
+  title = 'Webapp de Angular 4 con Angular CLI corriendo !!!';
 }
---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'empleado-tag',
+    templateUrl: './empleado.component.html'
+})
+export class EmpleadoComponent{
+    public titulo = 'titulo del componente empleado';
+}
